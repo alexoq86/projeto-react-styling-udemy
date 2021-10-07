ab7284cbbd2ddea22b36d9f6c3b93351a56e9621
Alexandre Oliveira Queiroz <alezoq86@gmail.com>
2021-10-07 14:05:55 -0400

    Form usando Styled Components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
+    { text: 'Faça todos os exercícios!', id: 'g1' },
+    { text: 'Termine o curso', id: 'g2' }
   ]);
 
   const addGoalHandler = enteredText => {
@@ -26,7 +26,7 @@ const App = () => {
   };
 
   let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
+    <p style={{ textAlign: 'center' }}>Sem objetivos. Talvez adicione um?</p>
   );
 
   if (courseGoals.length > 0) {
